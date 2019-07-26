e8e9ee53ce1d62b3ca2207d34dc1f62751a653bf
Marcos Gugeler <marcosgugeler@gmail.com>
2019-07-26 19:06:12 -0300

    First version

---
+++ readme.txt
@@ -0,0 +1,26 @@
+=== Search by Post ID ===
+Contributors: rezendemarcos
+Tags: search, id, posts, pages, custom post types, media, admin
+Requires at least: 4.0
+Tested up to: 5.0.2
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Enables the user to search by post ID using the built-in search within the control panel. Works for all kinds of posts.
+
+== Description ==
+
+Enables the user to search by Post ID using the built-in search in all website. Works for all kinds of posts (posts, pages, custom post types and media).
+
+= Features: =
+* Works for all kinds of posts (regular posts, pages, custom post types and media).
+* No configuration needed.
+* Doesn't add javascript or css; it has virtually no impact whatsoever.
+* Front-end and back-end functionality.
+* Doesn't add any options or tables to the database.
+
+= How to use it: =
+Simply enter an ID into the search field. If a post with that ID is found, it will show up in the search result.
+
+You can even enter a list of IDs if you want to search multiple IDs. For instance "100, 200, 300".
---
+++ search-by-post-id.php
@@ -0,0 +1,44 @@
+<?php
+/*
+Plugin Name: Search by Post ID
+Description: Enables the user to search by Post ID using the built-in search in all website. Works for all kinds of posts (posts, pages, custom post types and media).
+Version: 1.0
+Author: Marcos Rezende
+Author URI: https://www.linkedin.com/in/rezendemarcos/
+*/
+new GugelerSearchById();
+
+class GugelerSearchById
+{
+	function __construct()
+	{
+		add_filter('posts_where', array($this, 'posts_where'));
+	}
+
+	function posts_where($where)
+	{
+		if(is_search())
+		{
+			$s = $_GET['s'];
+
+			if(!empty($s))
+			{
+				if(is_numeric($s))
+				{
+					global $wpdb;
+
+					$where = str_replace('(' . $wpdb->posts . '.post_title LIKE', '(' . $wpdb->posts . '.ID = ' . $s . ') OR (' . $wpdb->posts . '.post_title LIKE', $where);
+				}
+				elseif(preg_match("/^(\d+)(,\s*\d+)*\$/", $s)) // string of post IDs
+				{
+					global $wpdb;
+
+					$where = str_replace('(' . $wpdb->posts . '.post_title LIKE', '(' . $wpdb->posts . '.ID in (' . $s . ')) OR (' . $wpdb->posts . '.post_title LIKE', $where);
+				}
+			}
+		}
+
+		return $where;
+	}
+}
+?>
